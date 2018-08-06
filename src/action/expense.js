ce8cbffa34e9f52fa7d824935e598e50a74f8269
Kevin Hwangpo <hwangpo3@gmail.com>
2018-08-06 08:53:42 -0700

    still working on getting expense in

---
+++ src/action/expense.js
@@ -0,0 +1,23 @@
+import uuid from 'uuid/v4';
+
+// doing a different style of exporting this time around
+export const createExpense = ({ name, price, categoryId }) => ({
+  type: 'EXPENSE_CREATE',
+  payload: {
+    name,
+    price,
+    categoryId,
+    _id: uuid(),
+    createdOn: new Date(),
+  },
+});
+
+export const updateExpense = expense => ({
+  type: 'EXPENSE_UPDATE',
+  payload: expense,
+});
+
+export const removeExpense = expense => ({
+  type: 'EXPENSE_REMOVE',
+  payload: expense,
+});
---
+++ src/components/expense-form/expense-form.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './expense-form.scss';
+
+const defaultState = {
+  name: '',
+  price: 0, 
+};
+
+export default class ExpenseForm extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = this.props.category || defaultState;
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const categoryId = this.props.category ? this.props.category.id : this.props.expense.categoryId;
+    this.props.onComplete({
+      ...this.state,
+      categoryId,
+    });
+    this.setState(defaultState);
+  }
+
+  render() {
+    const { expense } = this.props;
+    const buttonText = expense ? 'Update Expense' : 'Create Expense';
+    const formText = expense ? `Update ${expense.name} Expense` : 'Create Expense';
+    return (
+      <form
+        className="expense-form"
+        data-cy="expense-form"
+        onSubmit={ this.handleSubmit }
+      >
+        <label htmlFor="name">{ formText }</label>
+        <input 
+          type="text"
+          name="name"
+          placeholder="Enter New Expense Name"
+          value={ this.state.name }
+          onChange={ this.handleChange }
+        />
+        <button type="submit"> {buttonText} </button>
+      </form>
+    );
+  }
+}
+
+ExpenseForm.PropTypes = {
+  onComplete: PropTypes.func,
+  category: PropTypes.object,
+  expense: PropTypes.object,
+};
